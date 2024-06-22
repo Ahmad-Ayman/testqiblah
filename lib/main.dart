8cfd008c94bc29388bd9bac459c0e6c526a71403
Ahmad-Ayman <ahmed.ayman1708@gmail.com>
2024-06-22 18:14:31 +0300

    ي

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:smooth_compass_plus/utils/src/compass_ui.dart';
 import 'package:permission_handler/permission_handler.dart' as perm;
 import 'package:location/location.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   // This widget is the root of your application.
   @override
@@ -60,181 +60,175 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-
-          child: SmoothCompassWidget(
-            loadingAnimation: Container(
-              width: 400,
-              height: 400,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/fullbackground.png'),
-                    fit: BoxFit.cover),
-              ),
-              child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 16,
-                  ),
-                  child: Center(
-                    child: CircularProgressIndicator(),
-                  )),
+        child: SmoothCompassWidget(
+          // forceGPS: false,
+          loadingAnimation: Container(
+            width: 400,
+            height: 400,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/fullbackground.png'),
+                  fit: BoxFit.cover),
             ),
-            errorLocationServiceWidget: Column(
+            child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Center(
+                  child: CircularProgressIndicator(),
+                )),
+          ),
+          errorLocationServiceWidget: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'خدمة تحديد المواقع غير مفعلة',
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    Location loc = Location();
+                    await loc.requestService().then((bool value) {
+                      if (value) {
+                        setState(() {});
+                      }
+                    });
+                  },
+                  child: Text('تفعيل الخدمة'))
+            ],
+          ),
+          errorLocationPermissionWidget: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  'خدمة تحديد المواقع غير مفعلة',
-
+                  Platform.isAndroid
+                      ? 'من فضلك قم بقبول طلب الوصول لموقعك الحالي لتستطيع '
+                          'الوصول لاتجاه القبلة'
+                      : 'من فضلك اسمح '
+                          'للتطبيق بالوصول لخدمة تحديد الموقع من '
+                          'الاعدادات الخاصة بجهازك',
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 16,),
-
+                SizedBox(
+                  height: 16,
+                ),
                 ElevatedButton(
                     onPressed: () async {
                       Location loc = Location();
-                      await loc.requestService().then((bool value) {
-                        if (value) {
-                          setState(() {});
-                        }
-                      });
+                      if (Platform.isIOS) {
+                        await perm.openAppSettings().then((bool value) {
+                          if (value) {
+                            setState(() {});
+                          }
+                        });
+                      } else {
+                        await loc
+                            .requestPermission()
+                            .then((PermissionStatus value) {
+                          if (value == PermissionStatus.granted ||
+                              value == PermissionStatus.grantedLimited) {
+                            setState(() {});
+                          }
+                        });
+                      }
                     },
                     child: Text('تفعيل الخدمة'))
               ],
             ),
-            errorLocationPermissionWidget: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          ),
+          rotationSpeed: 200,
+          height: 500,
+          isQiblahCompass: true,
+          width: 500,
+          compassBuilder: (BuildContext context,
+              AsyncSnapshot<CompassModel>? compassData, Widget compassAsset) {
+            if (compassData?.data == null) {
+              return Center(
+                child: Text('Error: Unable to get compass data'),
+              );
+            }
+            return SizedBox(
+              height: 450,
+              width: 450,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    Platform.isAndroid
-                        ? 'من فضلك قم بقبول طلب الوصول لموقعك الحالي لتستطيع '
-                        'الوصول لاتجاه القبلة'
-                        : 'من فضلك اسمح '
-                        'للتطبيق بالوصول لخدمة تحديد الموقع من '
-                        'الاعدادات الخاصة بجهازك',
-                    textAlign: TextAlign.center,
+                  Expanded(
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          top: 0,
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          child: compassData!.data!.qiblahOffset.toInt() ==
+                                  compassData.data!.angle.toInt()
+                              ? Image.asset('assets/images/kaabafixed.png')
+                              : Image.asset('assets/images/qiblahright.png'),
+                        ),
+                        //put your qiblah needle here
+                        Positioned(
+                          top: 20,
+                          left: 0,
+                          right: 0,
+                          bottom: 20,
+                          child: AnimatedRotation(
+                            turns: compassData.data?.turns ?? 0 / 360,
+                            duration: const Duration(milliseconds: 400),
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  top: 20,
+                                  bottom: 20,
+                                  left: 0,
+                                  right: 0,
+                                  child: AnimatedRotation(
+                                      turns: (compassData.data!.qiblahOffset ??
+                                              0) /
+                                          360,
+                                      duration:
+                                          const Duration(milliseconds: 400),
+                                      child: Image.asset(
+                                          'assets/images/neeedle.png')),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 16,),
-                  ElevatedButton(
-                      onPressed: () async {
-                        Location loc = Location();
-                        if (Platform.isIOS) {
-                          await perm.openAppSettings().then((bool value) {
-                            if (value) {
-                              setState(() {});
-                            }
-                          });
-                        } else {
-                          await loc.requestPermission().then((
-                              PermissionStatus value) {
-                            if (value == PermissionStatus.granted ||
-                                value == PermissionStatus.grantedLimited) {
-                              setState(() {});
-                            }
-                          });
-                        }
-                      },
-                      child: Text('تفعيل الخدمة'))
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 80),
+                    child: Text(
+                      compassData.data!.qiblahOffset.toInt() ==
+                              compassData.data!.angle.toInt()
+                          ? '''أنت الآن في إتجاه القِبلة..
+ليتقبل الله منك'''
+                          : ''' قم بتدوير الهاتف حتى تصبح الكعبة في المنتصف''',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ],
               ),
-            ),
-            rotationSpeed: 400,
-            height: 500,
-            isQiblahCompass: true,
-            width: 500,
-            compassBuilder: (BuildContext context,
-                AsyncSnapshot<CompassModel>? compassData, Widget compassAsset) {
-              if (compassData?.data == null) {
-                return Center(
-                  child: Text('Error: Unable to get compass data'),
-                );
-              }
-              return SizedBox(
-                height:450,
-                width: 450,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: Stack(
-                        children: <Widget>[
-                          Positioned(
-                            top: 0,
-                            left: 0,
-                            right: 0,
-                            bottom: 0,
-                            child: compassData!.data!.qiblahOffset.toInt() ==
-                                compassData.data!.angle.toInt()
-                                ? Image.asset('assets/images/kaabafixed.png')
-                                : Image.asset('assets/images/qiblahright.png'),
-                          ),
-                          //put your qiblah needle here
-                          Positioned(
-                            top: 20,
-                            left: 0,
-                            right: 0,
-                            bottom: 20,
-                            child: AnimatedRotation(
-                              turns: compassData.data?.turns ?? 0 / 360,
-                              duration: const Duration(milliseconds: 400),
-                              child: Stack(
-                                children: <Widget>[
-                                  Positioned(
-                                    top: 20,
-                                    bottom: 20,
-                                    left: 0,
-                                    right: 0,
-                                    child: AnimatedRotation(
-                                        turns: (compassData.data!
-                                            .qiblahOffset ?? 0) /
-                                            360,
-                                        duration: const Duration(
-                                            milliseconds: 400),
-                                        child: Image.asset(
-                                            'assets/images/neeedle.png')),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-
-                    SizedBox(height: 16,),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 80),
-                      child: Text(
-                        compassData.data!.qiblahOffset.toInt() ==
-                            compassData.data!.angle.toInt()
-                            ? '''أنت الآن في إتجاه القِبلة..
-ليتقبل الله منك'''
-                            : ''' قم بتدوير الهاتف حتى تصبح الكعبة في المنتصف''',
-
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-
+            );
+          },
+        ),
       ),
-
     );
   }
 }
